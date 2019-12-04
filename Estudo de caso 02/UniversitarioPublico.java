d3ab666a30b059ec4ce2446ce4ee981b5a049d42
Cintia Braulino <cintiabraulino@gmail.com>
2019-12-04 19:45:10 -0300

    Add files via upload

---
+++ Estudo de caso 02/UniversitarioPublico.java
@@ -0,0 +1,22 @@
+
+public class UniversitarioPublico extends Universitario 
+{
+	UniversitarioPublico(String n)
+	{
+		System.out.println(n);
+	}
+	private double notaIra;
+
+	public double getNotaIra() 
+	{
+		return notaIra;
+	}
+
+	public void setNotaIra(double notaIra)
+	{
+		this.notaIra = notaIra;
+	}
+	
+	
+
+}
